f08d5a0b5f2d4d0c8a9cdf2d614f7c2c21a89591
Paul Manning <psmanningjr@gmail.com>
2021-04-16 15:13:21 -0400

    Load from Spring Initializr

---
+++ src/main/java/com/group6/jBravo/JBravoApplication.java
@@ -0,0 +1,13 @@
+package com.group6.jBravo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBravoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBravoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/jBravo/JBravoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.jBravo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBravoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
